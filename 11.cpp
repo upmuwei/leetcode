9caa2100feb1eb31c9e15afa043349778b277e77
muwei <lingjunmu@foxmail.com>
2022-07-06 16:13:17 +0800

    2022.7.6刷题

--- 11.cpp
+++ 11.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <vector>
 
+
+//双指针算法
+using namespace std;
 int main()
 {
-    
+    vector<int> height;
+    int n;
+    cin >> n;
+    while(n--) {
+        int t;
+        cin >> t;
+        height.push_back(t); 
+    }    
+    int max = 0;
+    int begin = 0, end = height.size() - 1;
+    while(begin != end) {
+        int sign = 0;
+        //条件运算符使用，括号、逗号后面的才参与赋值运算
+        int temp = (height[begin] > height[end] ? (sign = 1, height[end]):(sign = 0, height[begin])) * (end - begin);
+        if(temp > max)
+            max = temp;
+        if(sign) {
+            end--;
+        }else {
+            begin++;
+        }
+    }
+    cout << max << endl;
     return 0;
 }
---
+++ 12.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+//整数转罗马数字
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    map<int, char> transform;
+    transform[1] = 'I';
+    transform[5] = 'V';
+    transform[10] = 'X';
+    transform[50] = 'L';
+    transform[100] = 'C';
+    transform[500] = 'D';
+    transform[1000] = 'M';
+    string roman;
+    int t = x /1000, divied = 100;
+    x = x%1000;
+    for(int i = 0; i < t; i++)
+        roman.push_back('M');
+    while(x) {
+        t = x / divied;
+        x = x % divied;
+        if(t == 9) {
+            roman.push_back(transform[divied]);
+            roman.push_back(transform[10 * divied]);
+            t = 0;
+        } else if(t == 4) {
+            roman.push_back(transform[divied]);
+            roman.push_back(transform[5 * divied]);
+            t = 0;
+        } else if(t >= 5) {
+            roman.push_back(transform[5 * divied]);
+            t -= 5;
+        }
+        for(int i = 0; i< t; i ++) {
+            roman.push_back(transform[divied]);
+        }
+        divied /= 10;
+    }
+    cout << roman << endl;
+    return 0;
+}
