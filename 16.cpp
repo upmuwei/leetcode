29f47c8a55363256fa5d4c88c686acb808d4a4c9
muwei <lingjunmu@foxmail.com>
2022-07-12 10:11:25 +0800

    12日早上刷题

---
+++ 16.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    int target = 1, i = 0;
+    int result = 10000;
+    int a[1001];
+    cin >> n;
+    while(i < n) {
+        cin >> a[i];
+        i++;
+    } 
+    vector<int> nums(a, a + n);
+    sort(nums.begin(), nums.end());
+    int length = nums.size();
+    for(int i = 0; i < length; i++) {
+        int j = i + 1;
+        int k = length - 1;
+        while(j < k) {   
+            int t = nums[i] + nums[j] + nums[k] - target;
+            if(abs(t) < abs(result)) {
+                result = t;
+            }
+            if(t == 0) {
+                break;
+            }else if( t > 0) {
+                k--;
+            }else {
+                j++;
+            }
+        }
+        if(j != k) {
+            break;
+        }
+    }
+    cout << result + target << endl;
+    return 0;
+}
