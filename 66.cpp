2a50fa6a1ed900c3c2c3ae624127b3d96f288484
muwei <lingjunmu@foxmail.com>
2022-07-27 23:21:38 +0800

    7月27日23:21

---
+++ 66.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int t = 1;
+        for(int i = digits.size() - 1; i >= 0; i--) {
+            t += (digits[i]);
+            if(t < 10) {
+                digits[i] = t;
+                t = 0;
+                break;
+            } else {
+                digits[i] = 0;
+                t = 1;
+            }
+        }
+        if(t == 1) {
+            digits.insert(digits.begin(), t);
+        }
+        return digits;
+    } 
+};
